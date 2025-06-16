5d59ea061b7607583434b16e5c9f13e0fe654084
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-06-16 18:05:51 +0530

    Excercise1-Implementing a singleton Pattern added

---
+++ Solutions-DotNet_FSE/Week-1/exercise1.java
@@ -0,0 +1,17 @@
+class Singleton {
+    static Singleton instance = new Singleton(); 
+    
+    private Singleton() {
+        System.out.println("Singleton Created.");
+    }
+
+    void helloWorld() {
+        System.out.println("Hello World!");
+    }
+}
+
+public class exercise1 {
+    public static void main(String[] args) {
+        Singleton.instance.helloWorld();
+    }
+}
