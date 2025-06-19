b5333fae10175c4216f65f5194e55fe3f438f14a
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-06-19 14:09:08 +0530

    exercise1: Ranking and Windows Fn completed

--- Solutions-DotNet_FSE/Week-2/Mandatory-Exercises/Exercise1-Ranking_and_Windows_Fn/exercise1.sql
+++ Solutions-DotNet_FSE/Week-2/Mandatory-Exercises/Exercise1-Ranking_and_Windows_Fn/exercise1.sql
@@ -10,3 +10,26 @@ SELECT * FROM (
 ) AS RankedProducts
 WHERE RowNum <= 3;
 
+/* RANK() */
+SELECT * FROM (
+    SELECT 
+        ProductID,
+        ProductName,
+        Category,
+        Price,
+        RANK() OVER (PARTITION BY Category ORDER BY Price) AS RowNum
+    FROM Products
+) AS Ranked
+WHERE RowNum <= 3;
+
+/* DENSE_RANK() */
+SELECT * FROM (
+    SELECT 
+        ProductID,
+        ProductName,
+        Category,
+        Price,
+        DENSE_RANK() OVER (PARTITION BY Category ORDER BY Price) AS RowNum
+    FROM Products
+) AS RankedProd
+WHERE RowNum <= 3;
