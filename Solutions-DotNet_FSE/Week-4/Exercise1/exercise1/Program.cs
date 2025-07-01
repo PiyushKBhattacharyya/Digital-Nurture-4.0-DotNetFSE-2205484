93280dba7fed31129d6067a1f995cfb05a631199
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-07-01 13:44:38 +0530

    week4: ex1-Middleware support removed (originally added for SwaggerUI since controller was having some bugs)

--- Solutions-DotNet_FSE/Week-4/Exercise1/exercise1/Program.cs
+++ Solutions-DotNet_FSE/Week-4/Exercise1/exercise1/Program.cs
@@ -2,11 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-app.UseAuthorization();
 
 // Map controllers
 app.MapControllers();
